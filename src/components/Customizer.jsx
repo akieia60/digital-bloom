c7ba1b47d11317f130819440c53bd738e08fb0b8
Akieia Davis <akieia60@gmail.com>
2026-03-08 12:17:38 -0500

    chore: Temporarily remove text delivery option until SMS backend is built

--- src/components/Customizer.jsx
+++ src/components/Customizer.jsx
@@ -67,7 +67,7 @@ const Customizer = ({ product, isOpen, onClose, onComplete }) => {
   ];
 
   const symbolOptions = ['rose', 'cross'];
-  const deliveryMethods = ['email', 'text'];
+  const deliveryMethods = ['email'];
   const deliveryTimings = ['now', 'later', 'send-to-self-first'];
 
   // Dynamic Theme Engine Bridge
